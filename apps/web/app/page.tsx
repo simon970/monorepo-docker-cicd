9b3dec45ceefa9849d15b0b734a53ac0228ff461
simon970 <107906302+simon970@users.noreply.github.com>
2026-08-09 22:56:09 -0500

    made some changes for db migration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
 
 import {prismaClient} from "db/client";
 export default async function Home() {
-  const users:[] = await prismaClient.user.findMany();
+  const users:any = await prismaClient.user.findMany();
   return (
     <div >
      {JSON.stringify(users)}
     </div>
   );
 }
+export const dynamic = "force-dynamic";
